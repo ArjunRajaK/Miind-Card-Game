2c44a2063e2ba0b580c88c44de66368f8ca24fbe
Arjun K <ararjun596@gmail.com>
2025-04-03 18:25:49 +0530

    route update for removed component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,9 @@
 import { Routes } from '@angular/router';
 import { GridComponent } from './components/grid/grid.component';
-import { ResultComponent } from './components/result/result.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   {
     path: 'grid',
     component: GridComponent,
-  },
-  {
-    path: 'result',
-    component: ResultComponent,
   },
 ];
